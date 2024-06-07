dd4470b559415ae0b0d479b430a056d538994744
Libertocrat <libertocrat@proton.me>
2024-06-06 22:42:08 -0400

    Update: Finish Task 1

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -12,8 +12,8 @@ public_users.post("/register", (req,res) => {
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  // Return all books in database
+  return res.status(200).json({books: books});
 });
 
 // Get book details based on ISBN
