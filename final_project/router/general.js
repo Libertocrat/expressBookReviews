1cfab2a252fb47a0fb5f77fe137afe958c4af038
Libertocrat <libertocrat@proton.me>
2024-06-07 14:40:20 -0400

    Update: Complete Task 11

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -20,6 +20,26 @@ const getAllBooks = new Promise((resolve, reject) => {
     
 });
 
+// Retrieve a book by ISBN from the database, as an asyncronous function
+const getByISBN = (isbn) => new Promise( async (resolve, reject) => {
+
+    // Call & await the Promise to get all books, to avoid blocking
+    const allBooks = await getAllBooks;
+
+    // Check if ISBN is in the database
+    if (isbn in allBooks) {
+
+        // ISBN was found, resolve Promise with book object
+        let book = books[isbn];
+        resolve(book);
+    }
+    else {
+        // ISBN wasn't found, reject Promise with error message
+        reject(`ISBN '${isbn}' not found in database.`);
+    }
+    
+});
+
 /*** ROUTES ***/
 
 public_users.post("/register", (req,res) => {
@@ -53,7 +73,7 @@ public_users.post("/register", (req,res) => {
     
 });
 
-// Get the book list available in the shop asyncronously
+// Get the book list available in the shop, asyncronously
 public_users.get('/', async function (req, res) {
 
     try {
@@ -70,22 +90,26 @@ public_users.get('/', async function (req, res) {
   
 });
 
-// Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
+// Get book details based on ISBN, asyncronously
+public_users.get('/isbn/:isbn', async function (req, res) {
   
-  const isbn = req.params.isbn;
+    // Fetch ISBN from the request path
+    const isbn = req.params.isbn;
 
-  if (isbn in books) {
-    
-    let book = books[isbn];
-    return res.status(200).json(book);
-  }
-  else {
-    return res.status(404).json({message: `ISBN '${isbn}' not found in database`});
-  }
+    try {
+        // Call & await the Promise to get all books, to avoid blocking
+        const book = await getByISBN(isbn);
+
+        // Return book with matching ISBN, once the Promise has resolved
+        return res.status(200).json(book);
+    }
+    catch(error) {
+        // Respond with a "404 Not Found" if the Promise was rejected
+        return res.status(404).json({message: error});
+    }
   
- });
-  
+});
+
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   
