8fbcd3989eb49d891a359d96ff61a6eda9474e14
Libertocrat <libertocrat@proton.me>
2024-06-06 23:10:14 -0400

    Update: Finish Task 3

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -34,8 +34,14 @@ public_users.get('/isbn/:isbn',function (req, res) {
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  
+    // Parse requested author
+    const author = req.params.author;
+
+    // Extract just the books with the requested author
+    let filteredBooks = Object.values(books).filter((book) => book.author === author);
+
+    return res.status(200).json({booksbyauthor: filteredBooks});
 });
 
 // Get all books based on title
